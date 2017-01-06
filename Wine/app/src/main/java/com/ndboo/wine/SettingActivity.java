6017149ab286be4edb683beed50a2508010fd975
zhanghang <1083197619@qq.com>
2017-01-06 15:55:06 +0800

    我的

--- Wine/app/src/main/java/com/ndboo/wine/SettingActivity.java
+++ Wine/app/src/main/java/com/ndboo/wine/SettingActivity.java
@@ -1,9 +1,23 @@
 package com.ndboo.wine;
 
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.ndboo.base.BaseActivity;
 import com.ndboo.widget.TopBar;
 
+import java.io.File;
+import java.math.BigDecimal;
+
 import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * 设置页面
@@ -11,6 +25,30 @@ import butterknife.BindView;
 public class SettingActivity extends BaseActivity {
     @BindView(R.id.setting_topbar)
     TopBar mTopBar;
+    @BindView(R.id.setting_push_switch)
+    Switch mSwitch;
+    @BindView(R.id.setting_cache_size)
+    TextView mCacheTextView;
+    @BindView(R.id.setting_version_current)
+    TextView mVersionTextView;
+
+    //保存推送开启的关闭
+    private SharedPreferences mSharedPreferences;
+
+    @OnClick({R.id.setting_push, R.id.setting_cache, R.id.setting_version})
+    void doClick(View view) {
+        switch (view.getId()) {
+            case R.id.setting_push:
+                mSwitch.setChecked(!mSwitch.isChecked());
+                break;
+            case R.id.setting_cache:
+                clearAllCache();
+                break;
+            case R.id.setting_version:
+                Toast.makeText(this, "已是最新版本", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
 
     @Override
     public int getLayoutId() {
@@ -19,6 +57,15 @@ public class SettingActivity extends BaseActivity {
 
     @Override
     public void init() {
+        mSharedPreferences = getSharedPreferences("push_mode", MODE_PRIVATE);
+
+        //是否开启推送
+        boolean isEnablePush = mSharedPreferences.getBoolean("isEnablePush", true);
+        mSwitch.setChecked(isEnablePush);
+        //缓存大小
+        mCacheTextView.setText(getTotalCacheSize());
+        //当前版本
+        mVersionTextView.setText(getVersion());
         addListener();
     }
 
@@ -29,5 +76,122 @@ public class SettingActivity extends BaseActivity {
                 finish();
             }
         });
+
+        mSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+                SharedPreferences.Editor editor = mSharedPreferences.edit();
+                editor.putBoolean("isEnablePush", checked);
+                editor.commit();
+                String text = checked ? "开启" : "关闭";
+                Toast.makeText(SettingActivity.this, "已" + text + "推送", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    /**
+     * 获取缓存大小
+     */
+    public String getTotalCacheSize() {
+        long cacheSize = getFolderSize(getCacheDir());
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            cacheSize += getFolderSize(getExternalCacheDir());
+        }
+        return getFormatSize(cacheSize);
+    }
+
+    /**
+     * 清除缓存
+     */
+    public void clearAllCache() {
+        deleteDir(getCacheDir());
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            boolean isSuccess = deleteDir(getExternalCacheDir());
+            Toast.makeText(this, "清除缓存" + (isSuccess ? "成功" : "失败"), Toast.LENGTH_SHORT).show();
+            mCacheTextView.setText(getTotalCacheSize());
+        }
+    }
+
+    /**
+     * 删除缓存文件
+     */
+    private boolean deleteDir(File dir) {
+        if (dir != null && dir.isDirectory()) {
+            String[] children = dir.list();
+            for (int i = 0; i < children.length; i++) {
+                boolean success = deleteDir(new File(dir, children[i]));
+                if (!success) {
+                    return false;
+                }
+            }
+        }
+        return dir.delete();
+    }
+
+    // 获取文件大小
+    public long getFolderSize(File file) {
+        long size = 0;
+        try {
+            File[] fileList = file.listFiles();
+            for (int i = 0; i < fileList.length; i++) {
+                // 如果下面还有文件
+                if (fileList[i].isDirectory()) {
+                    size = size + getFolderSize(fileList[i]);
+                } else {
+                    size = size + fileList[i].length();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return size;
+    }
+
+    /**
+     * 格式化单位
+     */
+    public String getFormatSize(double size) {
+        double kiloByte = size / 1024;
+        if (kiloByte < 1) {
+            return size + "B";
+        }
+
+        double megaByte = kiloByte / 1024;
+        if (megaByte < 1) {
+            BigDecimal result1 = new BigDecimal(Double.toString(kiloByte));
+            return result1.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "K";
+        }
+
+        double gigaByte = megaByte / 1024;
+        if (gigaByte < 1) {
+            BigDecimal result2 = new BigDecimal(Double.toString(megaByte));
+            return result2.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "M";
+        }
+
+        double teraBytes = gigaByte / 1024;
+        if (teraBytes < 1) {
+            BigDecimal result3 = new BigDecimal(Double.toString(gigaByte));
+            return result3.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "G";
+        }
+        BigDecimal result4 = new BigDecimal(teraBytes);
+        return result4.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "T";
+    }
+
+    /**
+     * 获取版本号
+     *
+     * @return 当前应用的版本号
+     */
+    public String getVersion() {
+        PackageManager manager = this.getPackageManager();
+        PackageInfo info = null;
+        try {
+            info = manager.getPackageInfo(this.getPackageName(), 0);
+            String version = info.versionName;
+            return version;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
--- Wine/app/src/main/java/com/ndboo/wine/UserInfoActivity.java
+++ Wine/app/src/main/java/com/ndboo/wine/UserInfoActivity.java
@@ -1,9 +1,20 @@
 package com.ndboo.wine;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import com.ndboo.base.BaseActivity;
 import com.ndboo.widget.TopBar;
 
+import java.util.Calendar;
+import java.util.Locale;
+
 import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * 个人资料页面
@@ -11,6 +22,49 @@ import butterknife.BindView;
 public class UserInfoActivity extends BaseActivity {
     @BindView(R.id.userinfo_topbar)
     TopBar mTopBar;
+    @BindView(R.id.userinfo_nickname)
+    EditText mNickNameEditText;
+    @BindView(R.id.userinfo_birthday)
+    TextView mBirthdayTextView;
+
+    //获取一个日历对象
+    private Calendar mCalendar = Calendar.getInstance(Locale.CHINA);
+    //生日
+    private String mBirthday = "2017-01-06";
+
+    @OnClick({R.id.userinfo_birthday})
+    void doClick(View view) {
+        switch (view.getId()) {
+            case R.id.userinfo_birthday:
+                showDatePickerDialog();
+                break;
+        }
+    }
+
+    /**
+     * 生日选择弹框
+     */
+    private void showDatePickerDialog() {
+        //将生日字符串拆分成年月日
+        String[] dates = mBirthday.split("-");
+
+        final DatePickerDialog birthdayDialog = new DatePickerDialog(this,
+                AlertDialog.THEME_HOLO_LIGHT,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
+                        //修改日历控件的年，月，日
+                        String month = (monthOfYear + 1) < 10 ? ("0" + (monthOfYear + 1)) : ("" + (monthOfYear + 1));
+                        String day = (dayOfMonth < 10) ? ("0" + dayOfMonth) : ("" + dayOfMonth);
+                        mBirthday = year + "-" + month + "-" + day;
+                        mBirthdayTextView.setText(mBirthday);
+                    }
+                },
+                Integer.parseInt(dates[0]),
+                Integer.parseInt(dates[1]) - 1,
+                Integer.parseInt(dates[2]));
+        birthdayDialog.show();
+    }
 
     @Override
     public int getLayoutId() {
@@ -20,6 +74,7 @@ public class UserInfoActivity extends BaseActivity {
     @Override
     public void init() {
         addListener();
+        mBirthdayTextView.setText(mBirthday);
     }
 
     private void addListener() {
